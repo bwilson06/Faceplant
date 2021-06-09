f8df9128f0b89e22c464a28d00913cb0d97f394a
Bradley Wilson <bluedirtbike6@gmail.com>
2021-06-09 18:32:03 -0500

    add other logo svg files into react components, useEffect calls geocode and weather api, render weather icons depending on real time data, show current temp/weather as well as future temp/weather

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,118 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Logo from "../components/Logo";
+import LogoHappy from "../components/LogoHappy"
+import LogoAngry from "../components/LogoAngry"
 import { Button, ProgressBar } from "react-bootstrap";
 const Home = () => {
+  let [location, setLocation] = useState('Loading weather data')
+  let [currentTemp, setCurrentTemp] = useState({temp: '0˚', weather: 'Clear'})
+  let [hourOne, setHourOne] = useState('12 AM')
+  let [hourTwo, setHourTwo] = useState('12 AM')
+  let [hourThree, setHourThree] = useState('12 AM')
+  let [hourFour, setHourFour] = useState('12 AM')
+  let [hourOneTemp, setHourOneTemp] = useState({temp: '0˚', weather: 'Clear'})
+  let [hourTwoTemp, setHourTwoTemp] = useState({temp: '0˚', weather: 'Clear'})
+  let [hourThreeTemp, setHourThreeTemp] = useState({temp: '0˚', weather: 'Clear'})
+  let [hourFourTemp, setHourFourTemp] = useState({temp: '0˚', weather: 'Clear'})
+
+  let futureTimes = (time) => {
+    let t = time *= 1000;
+    let d = new Date(t).toLocaleTimeString(navigator.language, {hour: 'numeric'});
+    d = d.toLowerCase()
+    d = d.replace(/\s+/g, '')
+    return d;
+  }
+
+  let weatherIcons = (value) => {
+    let d = new Date(Date.now()).getHours()
+    if (d < 19){
+    switch (value) {
+      case 'Rain':
+        return (<i class="fas fa-cloud-sun-rain"></i>)
+      case 'Clear':
+        return (<i class="fas fa-sun"></i>)
+      case 'Clouds':
+        return (<i class="fas fa-cloud-sun"></i>)
+      default:
+        return (<i class="fas fa-sun"></i>)
+    }
+    }else{
+      switch (value) {
+        case 'Rain':
+          return (<i class="fas fa-cloud-moon-rain"></i>)
+        case 'Clear':
+          return (<i class="fas fa-moon"></i>)
+        case 'Clouds':
+          return (<i class="fas fa-cloud-moon"></i>)
+        default:
+          return (<i class="fas fa-sun"></i>)
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position) => {
+        let pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${pos.lat},${pos.lng}&result_type=postal_code&key=`)
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          if (data.results.length){
+            setLocation(data.results[0].formatted_address)
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+        
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${pos.lat}&lon=${pos.lng}&units=imperial&appid=`)
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          console.log(data)
+          let h1 = futureTimes(data.hourly[1].dt)
+          let h2 = futureTimes(data.hourly[2].dt)
+          let h3 = futureTimes(data.hourly[3].dt)
+          let h4 = futureTimes(data.hourly[4].dt)
+          let current = Math.floor(data.current.temp).toString()
+          current += '˚'
+          setCurrentTemp({temp: current, weather: data.current.weather[0].main})
+          setHourOne(h1)
+          setHourTwo(h2)
+          setHourThree(h3)
+          setHourFour(h4)
+          setHourOneTemp({temp: Math.floor(data.hourly[1].temp).toString() +'˚', weather: data.current.weather[0].main})
+          setHourTwoTemp({temp: Math.floor(data.hourly[2].temp).toString() +'˚', weather: data.current.weather[0].main})
+          setHourThreeTemp({temp: Math.floor(data.hourly[3].temp).toString() +'˚', weather: data.current.weather[0].main})
+          setHourFourTemp({temp: Math.floor(data.hourly[4].temp).toString() +'˚', weather: data.current.weather[0].main})
+        })
+        .catch((err) => {
+          if (err) console.log(err);
+        })
+        
+      })
+    }
+  },[])
+
+  let changeColor = (e) => {
+    e.preventDefault()
+    let thing = document.getElementsByClassName('progress-bar')[0].classList.add('yo')
+    console.log(thing)
+  }
+
   return (
     <div style={{ backgroundColor: "#282c34", height: "100vh" }}>
         <div style={{display: 'flex', justifyContent: 'flex-end'}}>
-        <h1 style={{color: 'white', marginRight: '10px', marginTop: '10px', marginBottom: '0px'}}><i class="fas fa-cog"></i></h1>
+        <h1 style={{color: 'white', marginRight: '10px', marginTop: '10px', marginBottom: '0px'}}><i class="fas fas-link fa-cog"></i></h1>
         </div>
-        <div style={{margin: '0 auto'}}>
-      <h1
+        <div className='text-center'style={{margin: '0 auto'}}>
+      <h2
         style={{
           color: "white",
           fontFamily: "Courier new, Sans serif",
@@ -16,53 +120,53 @@ const Home = () => {
           paddingBottom: '10px'
         }}
       >
-        Black Earth, WI
-      </h1>
-      <h1
+        {location}
+      </h2>
+      <h2
         style={{
           color: "white",
           fontFamily: "Courier new, Sans serif",
           textAlign: "center",
         }}
       >
-        72˚
-      </h1>
+        {currentTemp.temp}
+      </h2>
+      <p style={{position: 'absoulte', transform: 'translate(-40px,-38px)', color: 'white'}}>{weatherIcons(currentTemp.weather)}</p>
       </div>
       <div className="future-weather">
-          <div style={{fontSize: '20px', fontFamily: 'Courier new, Sans serif', paddingTop: '10px'}}>
-        <p>1am</p>
-        <i class="fas fa-moon"></i>
-        <p>72˚</p>
+          <div style={{fontSize: '20px', fontFamily: 'Courier new, Sans serif', paddingTop: '10px', textAlign: 'center'}}>
+        <p>{hourOne}</p>
+        {weatherIcons(hourOneTemp.weather)}
+        <p>{hourOneTemp.temp}</p>
         </div>
-        <div style={{fontSize: '20px', fontFamily: 'Courier new, Sans serif', paddingTop: '10px'}}>
-        <p>2am</p>
-        <i class="fas fa-cloud-moon"></i>
-        <p>70˚</p>
+        <div style={{fontSize: '20px', fontFamily: 'Courier new, Sans serif', paddingTop: '10px', textAlign: 'center'}}>
+        <p>{hourTwo}</p>
+        {weatherIcons(hourTwoTemp.weather)}
+        <p>{hourTwoTemp.temp}</p>
         </div>
-        <div style={{fontSize: '20px', fontFamily: 'Courier new, Sans serif', paddingTop: '10px'}}>
-        <p>3am</p>
-        <i class="fas fa-moon"></i>
-        <p>70˚</p>
+        <div style={{fontSize: '20px', fontFamily: 'Courier new, Sans serif', paddingTop: '10px', textAlign: 'center'}}>
+        <p>{hourThree}</p>
+        {weatherIcons(hourThreeTemp.weather)}
+        <p>{hourThreeTemp.temp}</p>
         </div>
-        <div style={{fontSize: '20px', fontFamily: 'Courier new, Sans serif', paddingTop: '10px'}}>
-        <p>4am</p>
-        <i class="fas fa-cloud-moon"></i>
-        <p>70˚</p>
+        <div style={{fontSize: '20px', fontFamily: 'Courier new, Sans serif', paddingTop: '10px', textAlign: 'center'}}>
+        <p>{hourFour}</p>
+        {weatherIcons(hourFourTemp.weather)}
+        <p>{hourFourTemp.temp}</p>
         </div>
           
       </div>
       <div
         className="text-center"
-        style={{ margin: "0 auto", top: "50%", transform: "translateY(10%)" }}
+        style={{ margin: "0 auto", top: "50%", transform: "translateY(6%)" }}
       >
-        <Logo />
+        <LogoAngry />
         <div style={{ width: "50%", margin: "0 auto", marginBottom: "40px" }}>
           <ProgressBar
-            style={{ backgroundColor: "#00cc66 !important" }}
-            now={100}
+            now={10}
           />
         </div>
-        <Button variant="outline-primary" size="lg" block>
+        <Button onClick={(e) => changeColor(e)} variant="outline-primary" size="lg" block>
           Give me Water!
         </Button>
       </div>
@@ -88,16 +192,16 @@ const Home = () => {
           }}
         >
           <h1>
-            <i class="fas fa-home"></i>
+            <i class="fas fas-link fa-home"></i>
           </h1>
           <h1>
-            <i class="fas fa-seedling"></i>
+            <i class="fas  fas-link fa-seedling"></i>
           </h1>
           <h1>
-            <i class="fas fa-signal"></i>
+            <i class="fas  fas-link fa-signal"></i>
           </h1>
           <h1>
-            <i class="fas fa-gamepad"></i>
+            <i class="fas fas-link fa-gamepad"></i>
           </h1>
         </div>
       </div>
